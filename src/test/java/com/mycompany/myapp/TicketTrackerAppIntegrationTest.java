3dfdf5bcf55119cd9c3a039d11702938f603566b
My Cle <107011917+Systemoutpri@users.noreply.github.com>
2025-07-30 16:13:57 +0800

    commit

--- src/test/java/com/mycompany/myapp/TicketTrackerAppIntegrationTest.java
+++ src/test/java/com/mycompany/myapp/TicketTrackerAppIntegrationTest.java
@@ -2,13 +2,12 @@ package com.mycompany.myapp;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 
 /**
  * Integration tests for the {@link TicketTrackerApp}.
  * This test verifies that the Spring application context can be loaded properly.
  */
-@SpringBootTest(classes = TicketTrackerApp.class)
+@IntegrationTest
 class TicketTrackerAppIntegrationTest {
 
     @Test
